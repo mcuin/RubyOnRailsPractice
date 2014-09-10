419942d95a09710d727b3df9bee4979205ce4fa0
Mykal Cuin <mykcuin@gmail.com>
2014-09-10 12:35:02 -0600

    Inital remote commit. Updated gem file for tutorial.

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '80b28d6c30ad79e9a88188af0c9d9ca7eb53f22d8d9f34038b53717ccbc7da39818965fce53b59b485fa3d4377929185f71f39e9f44cf9ad02ad8c461033c81f'
